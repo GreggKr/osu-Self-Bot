5eb5a36f1bad928d3e74b80aac30f4d416703961
gregg <greggkrzykowski@gmail.com>
2017-08-04 22:48:35 -0500

    Created MessageColor object for new color system

---
+++ src/main/java/me/sirgregg/osubot/util/config/MessageColor.java
@@ -0,0 +1,17 @@
+package me.sirgregg.osubot.util.config;
+
+public class MessageColor {
+	private int r, g, b;
+
+	public int getR() {
+		return r;
+	}
+
+	public int getG() {
+		return g;
+	}
+
+	public int getB() {
+		return b;
+	}
+}
