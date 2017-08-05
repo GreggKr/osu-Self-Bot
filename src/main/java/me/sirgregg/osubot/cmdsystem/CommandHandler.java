679defbc12901265f09b8f6536f28ae7623a1d32
gregg <greggkrzykowski@gmail.com>
2017-08-05 10:48:03 -0500

    Added call to AverageStarCommand

--- src/main/java/me/sirgregg/osubot/cmdsystem/CommandHandler.java
+++ src/main/java/me/sirgregg/osubot/cmdsystem/CommandHandler.java
@@ -1,6 +1,7 @@
 package me.sirgregg.osubot.cmdsystem;
 
 import me.sirgregg.osubot.OsuBot;
+import me.sirgregg.osubot.cmdsystem.commands.AverageStarCommand;
 import me.sirgregg.osubot.cmdsystem.commands.BeatmapCommand;
 import me.sirgregg.osubot.cmdsystem.commands.BestCommand;
 import me.sirgregg.osubot.cmdsystem.commands.UserCommand;
@@ -23,6 +24,7 @@ public class CommandHandler extends ListenerAdapter {
     	addCommand(new UserCommand());
     	addCommand(new BeatmapCommand());
     	addCommand(new BestCommand());
+    	addCommand(new AverageStarCommand());
     }
 
     public static List<Command> getCommands() {
